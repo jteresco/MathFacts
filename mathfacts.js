f25ed8c2db85953ba79bbd09473f2b05b3093c12
Jim Teresco <terescoj@gmail.com>
2018-08-24 10:02:16 -0400

    Basic countdown timer.

--- mathfacts.js
+++ mathfacts.js
@@ -1,11 +1,13 @@
 var problems = [];
 var currentProblem;
+var timeLimit = 5;
+var timeLeft;
 
 function initialize() {
 
     // create the list of problems
-    for (let a = 2; a <= 10; a++) {
-	for (let b = 2; b <= 10; b++) {
+    for (let a = 3; a <= 10; a++) {
+	for (let b = 3; b <= 10; b++) {
 	    problems.push({
 		problem: a + " x " + b,
 		answer: a*b
@@ -31,6 +33,32 @@ function pickProblem() {
     currentProblem = problems[problemNum];
     document.getElementById("problem").innerHTML = currentProblem.problem;
     document.getElementById("answer").innerHTML = "";
+
+    document.getElementById("message").innerHTML = "You have " + timeLimit + " seconds.";
+    timeLeft = timeLimit;
+    document.getElementById("leftToAnswer").innerHTML = "Problems left: " + problems.length;
+
+    // update countdown in 1 second
+    setTimeout(clockTick, 1000);
     
 }
 
+function clockTick() {
+
+    timeLeft--;
+    if (timeLeft == 0) {
+	// time's up
+	document.getElementById("answer").value = currentProblem.answer;
+	document.getElementById("message").innerHTML = "Time's up!  Remember for next time! " + currentProblem.problem + " = " + currentProblem.answer;
+    }
+    else {
+	if (timeLeft == 1) {
+	    document.getElementById("message").innerHTML = "You have 1 second.";
+	}
+	else {
+	    document.getElementById("message").innerHTML = "You have " + timeLeft + " seconds.";
+	}
+	setTimeout(clockTick, 1000);
+	
+    }
+}
